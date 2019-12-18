647b668318567a322175f6c34c932cd58b0a40d7
Chintan Patel <chintanpatel220598@gmail.com>
2019-12-18 12:02:56 +0530

    Add files via upload

---
+++ adder.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  6 00:23:37 2019
+Function file that adds two numbers/strings/lists...
+Default values of arguments
+@author: CHINTAN
+"""
+def add(x,y):
+    return(x+y)
+print("added integers:",add(1,3))    
+a="hello"
+b="world"
+print("added strings:",add(a,b))
+a=[1,2,3]
+b=[4,5,6]
+print("added list:",add(a,b))
+a=float(1.655)
+b=float(4.639)
+print("added float:",add(a,b))
+
+    
+      
+        
+    
+
